a3a46d4cc8dd93d9242d68913c0b08c71a2af94c
milkncookiesss <p.parkcontact@gmail.com>
2018-12-02 16:32:57 -0800

    "contact added with links"

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -4,15 +4,29 @@ import styled from 'styled-components';
 const About = (props) => {
   return (
     <div>
-      {/* <img>Github</img> */}
-      <Twitter src="https://s3-us-west-2.amazonaws.com/peter-park-portfolio-img/512px-Icon_Twitter.svg.png" href="https://www.twitter.com/milkncookiessss"></Twitter>
-      {/* <img>Linkedin</img> */}
+      <a href="https://www.github.com/milkncookiesss" target="_blank">
+      <Github src="https://s3-us-west-2.amazonaws.com/peter-park-portfolio-img/512px-Font_Awesome_5_brands_github.svg.png"></Github>
+      </a>
+      <a href="https://www.twitter.com/milkncookiessss" target="_blank">
+      <Twitter src="https://s3-us-west-2.amazonaws.com/peter-park-portfolio-img/512px-Icon_Twitter.svg.png"></Twitter>
+      </a>
+      <a href="https://www.linkedin.com/in/milkncookiesss" target="_blank">
+      <Linkedin src="https://s3-us-west-2.amazonaws.com/peter-park-portfolio-img/2000px-LinkedIn_logo_In-Black.svg.png"></Linkedin>
+      </a>
       {/* <img>Email</img> */}
     </div>
   )
 }
 
+const Linkedin = styled.img `
+  width: 5%;
+`;
+
+const Github = styled.img `
+  width: 5%;
+`;
+
 const Twitter = styled.img `
   width: 5%;
-`
+`;
 export default About;
