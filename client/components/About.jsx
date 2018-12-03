5cd6849a10d4c99345a61c5d3c91fd5ef442ac62
milkncookiesss <p.parkcontact@gmail.com>
2018-12-02 16:52:03 -0800

    "styling contact links"

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -7,26 +7,29 @@ const About = (props) => {
       <a href="https://www.github.com/milkncookiesss" target="_blank">
       <Github src="https://s3-us-west-2.amazonaws.com/peter-park-portfolio-img/512px-Font_Awesome_5_brands_github.svg.png"></Github>
       </a>
-      <a href="https://www.twitter.com/milkncookiessss" target="_blank">
-      <Twitter src="https://s3-us-west-2.amazonaws.com/peter-park-portfolio-img/512px-Icon_Twitter.svg.png"></Twitter>
-      </a>
       <a href="https://www.linkedin.com/in/milkncookiesss" target="_blank">
       <Linkedin src="https://s3-us-west-2.amazonaws.com/peter-park-portfolio-img/2000px-LinkedIn_logo_In-Black.svg.png"></Linkedin>
+      </a>
+      <a href="https://www.twitter.com/milkncookiessss" target="_blank">
+      <Twitter src="https://s3-us-west-2.amazonaws.com/peter-park-portfolio-img/512px-Icon_Twitter.svg.png"></Twitter>
       </a>
       {/* <img>Email</img> */}
     </div>
   )
 }
 
-const Linkedin = styled.img `
-  width: 5%;
+const Github = styled.img `
+width: 5%;
 `;
 
-const Github = styled.img `
+const Linkedin = styled.img `
+
   width: 5%;
 `;
 
 const Twitter = styled.img `
+  margin-right: auto;
+  margin-left: auto;
   width: 5%;
 `;
 export default About;
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,6 +3,7 @@ import Name from './Name.jsx';
 import About from './About.jsx';
 import Navbar from './Navbar.jsx';
 import Picture from './Picture.jsx';
+import styled from 'styled-components';
 
 class App  extends React.Component {
   constructor(props) {
@@ -21,11 +22,18 @@ class App  extends React.Component {
       <p>
         Header
       </p>
+      <AboutStyle>
       <About />
+      </AboutStyle>
       </div>
     );
   }
 }
 
+const AboutStyle = styled.div `
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+`
 
 export default App;
