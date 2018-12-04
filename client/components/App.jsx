9e16a823c49ede8f00d8335b47c6ddc27502cdae
milkncookiesss <p.parkcontact@gmail.com>
2018-12-03 20:19:58 -0800

    "hover animation and position for arrows"

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -10,7 +10,22 @@ import styled from 'styled-components';
 class App  extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      number: 0
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick(check) {
+    if (check === 'next') {
+      this.setState({
+        number: this.state.number + 1
+      });
+    } else if (check === 'prev') {
+      this.setState({
+        number: this.state.number - 1
+      });
+    }
   }
 
   render() {
@@ -29,7 +44,7 @@ class App  extends React.Component {
           <About />
         </div>
         <div>
-          <Applications />
+          <Applications number={this.state.number} onClick={this.handleClick}/>
         </div>
       <p>
         Header
--- client/components/Applications.jsx
+++ client/components/Applications.jsx
@@ -3,11 +3,61 @@ import styled from 'styled-components';
 
 const Applications = (props) => {
   return (
-    <div className="slideshow-container">
-      images go here
-    </div>
+    <Slideshow className="slideshow-container">
+      <div width="500px">
+        {props.number}
+      </div>
+      <Arrowsprev onClick={() => {props.onClick('prev')}} width="200px">&#10094;</Arrowsprev>
+      <Arrowsnext onClick={() => {props.onClick('next')}} width="200px">&#10095;</Arrowsnext>
+    </Slideshow>
     
   );
 }
 
+const Slideshow = styled.div `
+  max-width: 1000px;
+  position: relative;
+  margin: auto;
+`;
+
+const Arrowsprev = styled.a `
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  margin-top: -22px;
+  padding: 16px;
+  color: red;
+  font-weight: bold;
+  font-size: 20px;
+  transition: 0.6s ease;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+  &:hover {
+    background-color: rgba(0,0,0,0.4);
+  }
+`;
+const Arrowsnext = styled.a `
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  margin-top: -22px;
+  padding: 16px;
+  color: red;
+  font-weight: bold;
+  font-size: 20px;
+  transition: 0.6s ease;
+  right: 0;
+  border-radius: 3px 0 0 3px;
+  user-select: none;
+  &:hover {
+    background-color: rgba(0,0,0,0.4);
+  }
+`;
+/*
+  //change a tags to styled.a
+  //make them buttons with hover over background color
+  //replace {props.number} with images
+*/
 export default Applications;
